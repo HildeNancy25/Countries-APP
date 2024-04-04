b85e613dc10fdc2b6efe9edde4fe93c46bab204d
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-04-04 11:20:03 +0200

    changes

--- src/layouts/OtherLayouts.jsx
+++ src/layouts/OtherLayouts.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import Pagination from '../components/Pagination'
 
 const OtherLayouts = ({children}) => {
   return (
     <div>
       <NavBar />
       {children}
-      <Pagination />
       <Footer />
     </div>
   )
